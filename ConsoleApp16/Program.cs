a0cc10e4cef4b91b74e9462a8cfa33ef5b50d4c6
Павел <@WIN-7RHQ65MP88C>
2024-09-08 19:03:05 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace testWithGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
